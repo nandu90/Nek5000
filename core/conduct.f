88478910b24b5720d8a0ce46a78c57cca9ea0ed3
Nadish Saini <nsaini3@ncsu.edu>
2021-01-14 15:50:16 -0600

    added SVV for PS

--- core/conduct.f
+++ core/conduct.f
@@ -57,6 +57,9 @@ C
          if (ifaxis.and.ifaziv.and.ifield.eq.2) isd = 2
 c        if (ifaxis.and.ifmhd) isd = 2 !This is a problem if T is to be T!
 
+c     Replace with SVV Derivatives
+         call modifyDer(1)
+         
          do 1000 iter=1,nmxnl ! iterate for nonlin. prob. (e.g. radiation b.c.)
 
          intype = 0
@@ -65,6 +68,7 @@ c        if (ifaxis.and.ifmhd) isd = 2 !This is a problem if T is to be T!
          call bcneusc (ta,-1)
          call add2    (h2,ta,n)
          call bcdirsc (t(1,1,1,1,ifield-1))
+         call setmu_svv(t(1,1,1,1,ifield-1))
          call axhelm  (ta,t(1,1,1,1,ifield-1),h1,h2,imesh,ISD)
          call sub3    (tb,bq(1,1,1,1,ifield-1),ta,n)
          call bcneusc (ta,1)
@@ -96,6 +100,8 @@ C        Radiation case, smooth convergence, avoid flip-flop (ER).
  1000    continue
  2000    continue
 
+c     Restore derivatives
+         call modifyDer(-1)
       endif
 
       return
