bdd800704c011a7ee07ed572dbe0b7cea108b26c
Dillon Shaver <dshaver@anl.gov>
2021-04-16 15:41:51 -0500

    sets shl bcs to Dirichlet for rans scalars

--- core/experimental/rans_komg.f
+++ core/experimental/rans_komg.f
@@ -290,6 +290,9 @@ c set cbc array for k and omega/tau (need to revise for wall-functions)
         elseif(bcw.eq.'SYM'.or.bcw.eq.'O  '.or.bcw.eq.'o  ') then
           cbc(ifc,ie,ifld_k)='I  '
           cbc(ifc,ie,ifld_omega)='I  '
+        elseif(bcw.eq.'shl') then
+          cbc(ifc,ie,ifld_k)='t  '
+          cbc(ifc,ie,ifld_omega)='t  '
         endif
   10  continue
 
